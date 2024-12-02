5e610d147fdd4998f33e53139585eed58b77db81
NY Jan <nnyjan02426@gmail.com>
2024-12-02 10:30:49 +0800

    setup login page interaction

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,11 @@ import Chatroom from './pages/chatroom/Chatroom';
 import ChatList from './pages/chatList/ChatList';
 import Details from './pages/details/Details';
 import Login from './pages/login/Login';
+import { useState } from 'react';
 
 
 const App = () => {
+  // const [user, setUser] = useState(localStorage.getItem('token') ? true : false);
   const user = true;
   return (
     <div id='App'>
